f747c8167efac71a9e8a5febf40b1d82a2d2d3a2
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-01 12:38:20 +0400

    task tamam elave style

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,6 @@
 function Testomonials(props) {
     return <>
-        <div>
+        <div class="card">
             <img src={props.imgUrl} alt="" />
             <h1>Hi, I'm {props.name}</h1>
             <p>I'm {props.name} and i' m {props.age} years old</p>
